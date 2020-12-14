192103d0cf650dcbfa276771e02bf2b335695110
Lane Garner <b.lane.garner@gmail.com>
2020-12-14 12:10:29 -0600

    set up router in index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const router = require('./routes/employees.js')
 const port = 4001
 
+app.use(router)
 app.get('/', (req, res) => res.send('Welcome to our API'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
