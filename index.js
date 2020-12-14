d069ebfc5809194b82f18fba6b1925a4a73f0da8
Lane Garner <b.lane.garner@gmail.com>
2020-12-14 11:20:23 -0600

    added basic express

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('re-testing')
+const express = require('express')
+const app = express()
+const port = 4001
+
+app.get('/', (req, res) => res.send('Welcome to our API'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
