1d2d69026809408e82271be8cfcb519c578ba7e1
David-Hebert <king.david.abear@zoho.com>
2020-12-14 12:05:19 -0600

    slight adjustments

--- routes/employees.js
+++ routes/employees.js
@@ -2,8 +2,8 @@ const express = require('express')
 const usersController = require('../controllers/users')
 const router = express.Router()
 
-router.get('/')
-router.get('/:id')
-router.get('firstname/:first_name')
+router.get('/', controller.getAll)
+router.get('/:id', controller.getById)
+router.get('firstname/:first_name', controller.getByFirstName)
 
 module.exports = router
